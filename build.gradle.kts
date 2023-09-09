2509eeb482633f3928f72367b42884bfb50ffede
savrov <savrov.code@gmail.com>
2023-09-09 04:25:37 +0200

    GitHub Packages publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = URI.create("https://maven.pkg.github.com/octocat/hello-world")
+            url = URI.create("https://maven.pkg.github.com/credible-team/gradle-versions")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
