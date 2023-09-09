36417898a3c3f1b8597198274b3d565f917b3349
savrov <savrov.code@gmail.com>
2023-09-09 13:58:56 +0200

    update koin module

---
+++ sqldelight/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("org.gradle.version-catalog")
+    id("org.gradle.maven-publish")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("versionCatalog") {
+            from(components["versionCatalog"])
+            groupId = project.parent?.group?.toString()
+            artifactId = "versions-sqldelight"
+        }
+    }
+}
