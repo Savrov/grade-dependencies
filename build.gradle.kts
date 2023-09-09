376e1e9523e012604c13e1ae41f6b613eb9cfa2a
savrov <savrov.code@gmail.com>
2023-09-09 03:31:04 +0200

    GitHub Packages publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,19 @@
+import java.net.URI
+
 plugins {
     `version-catalog`
     `maven-publish`
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = URI.create("https://maven.pkg.github.com/octocat/hello-world")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
