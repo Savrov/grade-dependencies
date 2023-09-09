a091b73269551483d1088d36336bb9ae038e8f7c
savrov <savrov.code@gmail.com>
2023-09-09 04:54:00 +0200

    refactor gradle tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,31 @@
 import java.net.URI
 
 plugins {
-    `version-catalog`
-    `maven-publish`
+    id("org.gradle.version-catalog")
+    id("org.gradle.maven-publish")
+}
+
+subprojects {
+    apply(plugin = "org.gradle.version-catalog")
+    apply(plugin = "org.gradle.maven-publish")
+
+    catalog {
+        versionCatalog {
+            from(files("libs.versions.toml"))
+        }
+    }
+
+    publishing {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = URI.create("https://maven.pkg.github.com/credible-team/gradle-versions")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+    }
+
 }
--- jetbrains/build.gradle.kts
+++ jetbrains/build.gradle.kts
@@ -1,31 +1,9 @@
-import java.net.URI
-
 plugins {
-    `version-catalog`
-    `maven-publish`
-}
-
-repositories {
-    mavenCentral()
-}
-
-catalog {
-    versionCatalog {
-        from(files("libs.versions.toml"))
-    }
+    id("org.gradle.version-catalog")
+    id("org.gradle.maven-publish")
 }
 
 publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = URI.create("https://maven.pkg.github.com/credible-team/gradle-versions")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
     publications {
         create<MavenPublication>("versionCatalog") {
             from(components["versionCatalog"])
