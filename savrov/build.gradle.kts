577d9495c3843bf802f853267acf0dcf9204a5b0
savrov <savrov.code@gmail.com>
2024-01-02 00:35:42 +0100

    add savrov module + rename workflows

---
+++ savrov/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("org.gradle.version-catalog")
+    id("org.gradle.maven-publish")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("savrovVersionCatalog") {
+            from(components["versionCatalog"])
+            groupId = project.parent?.group?.toString()
+            artifactId = project.parent?.name + "-savrov"
+        }
+    }
+}
