023a827bfd8d9200bc4f5714f01129be7786d51b
Pavel <Savrov@users.noreply.github.com>
2024-01-12 20:56:56 +0100

    Unite all version catalogs into one (#5)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,19 +2,34 @@ rootProject.name = extra["name"] as String
 
 pluginManagement {
     repositories {
-        mavenCentral()
+        google()
         gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
-include("android")
-include("arkivanov")
-include("google")
-include("jetbrains")
-include("koin")
-include("kotest")
-include("ktor")
-include("other")
-include("savrov")
-include("sqldelight")
-include("supabase")
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.7.0")
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven {
+            url = uri("https://maven.pkg.github.com/Savrov/core-library-cleanarch")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+include("sample:client:mobile:android")
+include("sample:client:mobile:shared")
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
