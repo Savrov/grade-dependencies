a172688e6c99fb5781472564d7b7828916c1b78a
savrov <savrov.code@gmail.com>
2024-01-12 20:53:43 +0100

    Unite all version catalogs into one

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,27 +5,22 @@ plugins {
     id("org.gradle.maven-publish")
 }
 
-subprojects {
-    apply(plugin = "org.gradle.version-catalog")
-    apply(plugin = "org.gradle.maven-publish")
-
-    catalog {
-        versionCatalog {
-            from(files("libs.versions.toml"))
-        }
-    }
-
-    publishing {
-        repositories {
-            maven {
-                name = "GitHubPackages"
-                url = URI.create("https://maven.pkg.github.com/Savrov/grade-version-catalogs")
-                credentials {
-                    username = System.getenv("GITHUB_ACTOR")
-                    password = System.getenv("GITHUB_TOKEN")
-                }
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = URI.create("https://maven.pkg.github.com/Savrov/grade-version-catalogs")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
-
+    publications {
+        create<MavenPublication>("versionCatalog") {
+            from(components["versionCatalog"])
+            groupId = project.group.toString()
+            artifactId = project.name
+        }
+    }
 }
